59e73d4e0aea82aabbe7863ce15ce01b53afcada
pyemkey <py.emkey@gmail.com>
2014-01-23 23:50:54 +0100

    Finish Comment model & controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+	def mark_as_not_abusive
+		@post = Post.find(params[:post_id])
+		@comment = Comment.find(params[:id])
+		@comment.update_attributes(abusive: false)
+		redirect_to @post
+	end
+
+	def vote_up
+		@post = current_user.posts.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		if @comment.votes.empty?
+			@comment.votes.create(value: 1).save
+		end
+		redirect_to @post
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,33 @@
 class Comment
+	include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :body, type: String
+  field :abusive, type: Boolean, default: false
+	belongs_to :user
+	belongs_to :post
+	has_many :votes, after_add: :abusive_comment
+	scope :bad_votes, ->{ where(value: -1)} 
+
+	def unlock_abusive_state
+		self.abusive = false
+
+	end
+
+	def display_for_post_author
+		
+	end
+
+	def positive_value? (value)
+		value > 0
+	end
+
+	private
+		def abusive_comment(vote)
+			 if vote.value - self.votes.where("value = -1 ").count  == -3 
+			 	update_attribute 'abusive', true 
+			 end
+		end
+
+
 end
