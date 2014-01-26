a172960b9b0b8a544827e2de91656cbc6ca60771
pyemkey <py.emkey@gmail.com>
2014-01-26 23:42:07 +0100

    Update relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  has_many :comments
+  has_many :posts
+  has_many :votes
+   
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
@@ -26,13 +30,13 @@ class User
   field :last_sign_in_at,    type: Time
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
-
+  field :name, type: String
+  field :nickname, type: String
   index({ email: 1 }, { unique: true, background: true })
   validates :name, presence: true
   validates :nickname, uniqueness: true, if: proc { |u| u.nickname }
 
-  field :name, type: String
-  field :nickname, type: String
+  
 
   def owner? post
     posts.include? post
