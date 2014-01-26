1b1a2c8539792ea84b12abcdb3d2b1e37ffcafa5
pyemkey <py.emkey@gmail.com>
2014-01-26 23:14:41 +0100

    Fix problem with signup new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+end
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
