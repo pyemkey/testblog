e530924c6d4501a7f4396b18477e27e1ed69de35
pyemkey <py.emkey@gmail.com>
2014-01-29 19:43:02 +0100

    Fix double transaction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
 	def create
-		@comment = Comment.create(comment_params)
+		@comment = Comment.new(comment_params)
 		@comment.post_id = params[:post_id]
 		@comment.save
 		current_user.add_comment @comment
